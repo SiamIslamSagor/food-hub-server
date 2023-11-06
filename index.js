e0b7fe7e74ef9b203149521c70603e217f1a14b5
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-07 01:52:33 +0600

    created jwt token and send it in client side , also created verifyToken middleware to verify jwt token

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookeParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+// to read cookie data
+app.use(cookeParser());
+// to gate data in body
 app.use(express.json());
 
 console.log(process.env.DB_PASS);
@@ -22,12 +33,72 @@ const client = new MongoClient(uri, {
   },
 });
 
+// my own created middleware:
+// //////////////////////
+
+// token verify middleware
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  // if token not exist , then run this codeblock
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  //  if token exist then , verify the token in this code block
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    // if token have any problem, then run this codeblock
+    if (error) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    // if token have no problem, then run this codeblock
+    req.user = decoded;
+    next();
+  });
+};
+
+// //////////////////////
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     ///////////////////////////////////////////////
 
+    // database and collection
+    const foodsCollection = client
+      .db("foodHubDB")
+      .collection("foodsCollection");
+
+    // jwt auth api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      //  generate a new cookie token
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "2h",
+      });
+      // set token in cookie
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    // when called the api , then clear the cookie
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("LOGGING OUT=>>>>>", user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
+
+    app.get("/", async (req, res) => {
+      const result = await foodsCollection.find().toArray();
+      res.send(result);
+    });
     ///////////////////////////////////////////////
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -36,14 +107,10 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`FOODHUB SERVER IS RUNNING ON PORT ${port}`);
