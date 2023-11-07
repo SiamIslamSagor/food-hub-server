55ad6ef3b222743cf3f403f96aa032385cd1db32
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-07 14:53:34 +0600

    create post api to store new added food in database

--- index.js
+++ index.js
@@ -105,9 +105,15 @@ async function run() {
       res.send(result);
     });
 
+    // create: insert food in food collection
+    app.post('/', async(req, res)=> {
+      const data = req.body;
+      const result = await foodsCollection.insertOne(data);
+      res.send(result)
+    })
+
     // read: get single food
     app.get("/food/:id", async (req, res) => {
-      const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await foodsCollection.findOne(query);
       res.send(result);
